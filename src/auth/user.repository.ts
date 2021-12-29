85e6c955811f4d0cff27fb15c63082a4acd40f0b
Abdul Qoyyum <nurudeen96qoyyum@gmail.com>
2021-12-29 14:02:31 +0100

    Added password hashing algorithm

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,32 @@
+import { EntityRepository, Repository } from 'typeorm';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials-dto';
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    try {
+      const { username, password } = authCredentialsDto;
+      const user = new User();
+      user.username = username;
+      user.salt = await bcrypt.genSalt();
+      user.password = await this.hashPassword(password, user.salt);
+      await user.save();
+    } catch (e) {
+      if (e.code === '23505') {
+        //already existing user
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+}
